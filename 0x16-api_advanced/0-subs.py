096b23c8c59b442d2f6c8d42c0fa1ed0b25cda73
Amos Kiplimo <168921899+kismoflans@users.noreply.github.com>
2024-08-18 17:42:50 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-"""
-Defines a function that queries Reddit API
-"""
+# queries the Reddit API and returns the number of subscribers
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of
-    subscribers (not active users, total subscribers) for a given subreddit
-    """
-    subscribers = 0
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'advanced-api/0.0.1 by Mendy'}
-    req = requests.get(url=url, headers=headers, allow_redirects=False)
-    if req.status_code == 200:
-        response = req.json()
-        subscribers = response['data']['subscribers']
-    return subscribers
+    """return total subscribers of reddit's subreddit"""
+
+    url = 'https://www.reddit.com/r/' + subreddit + '/about.json'
+    try:
+        response = requests.get(url,
+                                headers={'User-agent': 'your bot 0.1'},
+                                allow_redirects=False).json()
+        return response['data'].get('subscribers')
+    except:
+        return 0
